bda51bffc1c0dfa07ebcfbaf3f94f190ac2f9889
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-04-15 11:19:19 +0100

    Avoided memory leaks, renamed function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,76 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgClass } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, FormGroup, AbstractControl, Validators, AbstractControlOptions } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, AbstractControl, Validators, AbstractControlOptions, FormControlStatus } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../user.service';
 import { BackendLoginValidator } from './backend-validation.validator';
-import { Subject, filter, startWith, switchMap, take } from 'rxjs';
+import { Observable, Subject, Subscription, filter, startWith, switchMap, take } from 'rxjs';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [
-    NgClass,
-    RouterLink,
-    ReactiveFormsModule,
-  ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+	selector: 'app-login',
+	standalone: true,
+	imports: [
+		NgClass,
+		RouterLink,
+		ReactiveFormsModule,
+	],
+	templateUrl: './login.component.html',
+	styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  protected form: FormGroup;
+export class LoginComponent implements OnDestroy {
+	protected form: FormGroup;
 	protected formSubmitSubject$: Subject<any>;
-  protected submitted: boolean = false;
+	protected submitted: boolean = false;
+	protected formStatusObservable$: Observable<FormControlStatus>;
+	protected formStatusSubscription: Subscription;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private userService: UserService,
+	constructor(
+		private formBuilder: FormBuilder,
+		private router: Router,
+		private userService: UserService,
 		private backendLoginValidator: BackendLoginValidator
-  ){
-    this.form = this.formBuilder.group({
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-    }, {
+	) {
+			this.form = this.formBuilder.group({
+				username: ['', [Validators.required]],
+				password: ['', [Validators.required]],
+			}, {
 				asyncValidators: backendLoginValidator.validate.bind(this),
 				updateOn: "submit"
-		} as AbstractControlOptions);
+			} as AbstractControlOptions);
 
-		this.formSubmitSubject$ = new Subject();
-		this.formSubmitSubject$
-		.pipe(
-			switchMap((value, index) => 
-				this.form.statusChanges.pipe(
-					startWith(this.form.status),
-					filter(status => status === 'VALID' || status === 'INVALID'),
-					take(1)
-				)
-			),
-		).subscribe((status) => {
-			this.submitted = true;
-			if (status === 'VALID') 
-				this.submitForm();
-		});
-  }
+			this.formSubmitSubject$ = new Subject();
 
-  protected get control(): { [key: string ]: AbstractControl} {
-    return this.form.controls;
-  }
+			this.formStatusObservable$ = this.formSubmitSubject$
+				.pipe(
+					switchMap((value, index) =>
+						this.form.statusChanges.pipe(
+							startWith(this.form.status),
+							filter(status => status === 'VALID' || status === 'INVALID'),
+							take(1)
+						)
+					),
+				);
 
-  submitForm(): void {
-      this.userService
-      .login({
-        username: this.form.value['username'],
-        password: this.form.value['password'],
-      })
-      .subscribe({
-        next: (data) => {
-          console.log(`${data.username} are login...`);
-          sessionStorage.setItem('user', JSON.stringify(data));
-        },
-        complete: () => {
-          this.router.navigateByUrl('/home');
-        },
-      });
-    }
-  }
+			this.formStatusSubscription = this.formStatusObservable$.subscribe((status) => {
+				this.submitted = true;
+				if (status === 'VALID')
+					this.logUser();
+			}
+			);
+	}
+
+
+	protected get control(): { [key: string]: AbstractControl } {
+		return this.form.controls;
+	}
+
+	logUser(): void {
+		this.userService
+			.login({
+				username: this.form.value['username'],
+				password: this.form.value['password'],
+			})
+			.subscribe({
+				next: (data) => {
+					console.log(`${data.username} are login...`);
+					sessionStorage.setItem('user', JSON.stringify(data));
+				},
+				complete: () => {
+					this.router.navigateByUrl('/home');
+				},
+			});
+	}
+	
+	ngOnDestroy(): void {
+		this.formStatusSubscription.unsubscribe();
+	}
+}
