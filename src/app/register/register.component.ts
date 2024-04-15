085dbcdb396b3ef77f757951105a0e9793ea6c3e
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-04-15 12:45:02 +0100

    Implemented async validation in register!!! 🕛

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,73 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgClass } from '@angular/common';
-
 import { Router, RouterLink } from '@angular/router';
-
 import { ReactiveFormsModule, FormBuilder, FormGroup, AbstractControl, Validators, AbstractControlOptions, FormControlStatus } from '@angular/forms';
-
 import { UserService } from '../user.service';
 import { passwordValidator } from './password-validator';
-import { Observable, Subject, Subscription } from 'rxjs';
+import { Observable, Subject, Subscription, filter, startWith, switchMap, take } from 'rxjs';
 import { BackendRegisterValidator } from './backend-reg-validation.validator';
+
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [
-    NgClass,
-    ReactiveFormsModule,
-    RouterLink,
-  ],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+	selector: 'app-register',
+	standalone: true,
+	imports: [
+		NgClass,
+		ReactiveFormsModule,
+		RouterLink,
+	],
+	templateUrl: './register.component.html',
+	styleUrl: './register.component.css'
 })
-export class RegisterComponent {
-  protected form: FormGroup;
+export class RegisterComponent implements OnDestroy {
+	protected form: FormGroup;
 	protected formSubmitSubject$: Subject<any>;
-  protected submitted: boolean = false;
+	protected submitted: boolean = false;
 	protected formStatusObservable$: Observable<FormControlStatus>;
 	protected formStatusSubscription: Subscription;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private userService: UserService,
+	constructor(
+		private formBuilder: FormBuilder,
+		private router: Router,
+		private userService: UserService,
 		private backendRegisterValidator: BackendRegisterValidator
-  ){
-    this.form = this.formBuilder.group({
-      username: ['', Validators.required],
-      email: ['', [Validators.email]],
-      password: ['', Validators.required],
-      passwordConfirm: ['', Validators.required],
-    }, {
+	) {
+		this.form = this.formBuilder.group({
+			username: ['', Validators.required],
+			email: ['', [Validators.email]],
+			password: ['', Validators.required],
+			passwordConfirm: ['', Validators.required],
+		}, {
 			validators: passwordValidator,
-			asyncValidators: backendRegisterValidator.validate.bind(this)
+			asyncValidators: backendRegisterValidator.validate.bind(this),
+			updateOn: 'submit'
 		} as AbstractControlOptions);
-  }
 
-  protected get control(): { [key: string ]: AbstractControl} {
-    return this.form.controls;
-  }
+		this.formSubmitSubject$ = new Subject();
 
-  onSubmit(): void {
-    this.submitted = true;
-    if (this.form.valid) {
-      this.userService
-      .register({
-        username: this.form.value['username'],
-        email: this.form.value['email'],
-        password: this.form.value['password'],
-        passwordConfirm: this.form.value['passwordConfirm']
-      })
-      .subscribe({
-        complete: () => {
-          console.log(`The user ${this.form.value['username']} has been registered!`);
-          this.router.navigateByUrl('/login');
-        },
-        error: (response) => {
-          console.log(`ERROR(${response.status}): ${response.error.message}`);
-          this.error = response.error.message;
-        }
-      });
-    }
-  }
+		this.formStatusObservable$ = this.formSubmitSubject$
+			.pipe(
+				switchMap((value, index) =>
+					this.form.statusChanges.pipe(
+						startWith(this.form.status),
+						filter(status => status === 'VALID' || status === 'INVALID'),
+						take(1)
+					)
+				),
+			);
+
+		this.formStatusSubscription = this.formStatusObservable$.subscribe((status) => {
+			this.submitted = true;
+			if (status === 'VALID')
+				this.registerUser();
+			}
+		);
+	}
+
+	protected get control(): { [key: string]: AbstractControl } {
+		return this.form.controls;
+	}
+
+	registerUser(): void {
+		this.userService
+			.register({
+				username: this.form.value['username'],
+				email: this.form.value['email'] ?? "pedro@gmail.com",
+				password: this.form.value['password'],
+				passwordConfirm: this.form.value['passwordConfirm']
+			})
+			.subscribe({
+				complete: () => {
+					console.log(`The user ${this.form.value['username']} has been registered!`);
+					this.router.navigateByUrl('/login');
+				},
+				error: (response) => {
+					console.log(`ERROR(${response.status}): ${response.error.message}`);
+				}
+			});
+	}
+
+	ngOnDestroy(): void {
+		this.formStatusSubscription.unsubscribe();
+	}
 }
